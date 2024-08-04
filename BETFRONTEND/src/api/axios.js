49d4268e296cacaae647e78d212ac40bc1b7a2b6
Androrun <106136947+Androrun@users.noreply.github.com>
2024-08-04 10:46:20 -0500

    Update axios.js

--- BETFRONTEND/src/api/axios.js
+++ BETFRONTEND/src/api/axios.js
@@ -1,9 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const client = axios.create ({
-    baseURL: 'http://localhost:3000/api',
-        withCredentials: true,
-    
+const client = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
 });
 
 export default client;
