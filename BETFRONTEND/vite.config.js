bef578dcec06ea5468254881fd41c003b766212d
Androrun <106136947+Androrun@users.noreply.github.com>
2024-08-03 20:10:31 -0500

    Update Vite

--- BETFRONTEND/vite.config.js
+++ BETFRONTEND/vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: './BETFRONTEND',  // Configura el directorio raíz
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: './index.html',
+      input: './BETFRONTEND/index.html',  // Asegúrate de que el camino sea correcto
     },
   },
 });
