779d20d956d915a34b0094cd636b65ebd9d8e8fc
Androrun <106136947+Androrun@users.noreply.github.com>
2024-08-04 13:40:23 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -43,10 +43,11 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000; // Railway proporcionará el puerto correcto
+const HOST = '0.0.0.0';
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+    console.log(`Server running on http://${HOST}:${PORT}`);
 });
 
 export default app;
