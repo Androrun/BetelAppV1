23edbc9dfc3f3f37438b8bdb4b97e87b6cb44c93
Androrun <106136947+Androrun@users.noreply.github.com>
2024-08-03 21:02:59 -0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,12 +1,12 @@
-import pg from 'pg'
+// src/db.js
+import pg from 'pg';
 
 export const pool = new pg.Pool({
-    port: 5432,
-    host: 'kala.db.elephantsql.com',
-    user: 'byxyrpin',
-    password: '7zbwSPqB02RugLpt_KTDWzzqT6wGVlXe',
-    database: 'byxyrpin'
-})
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 pool.on('connect', () => {
     console.log("Database connected");
